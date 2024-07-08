93d39ef2128b166287df2cdcff020059b3316450
YangJaegeon <jaeheony03@gmail.com>
2024-07-08 15:08:28 +0900

    user_pull

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
--- user/models.py
+++ user/models.py
@@ -4,39 +4,40 @@ from artwork.models import ArtWork
 from exhibit.models import ArtExhibit
 
 class User(AbstractUser):
+    # 기본 사용자 모델에는 username, password, email, first_name, last_name 등 기본적인 필드들이 포함.
+    # username은 식별자
     nickname = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to='photos/')
+    photo_url = models.URLField(null=True)
     phone_number = models.CharField(max_length=15)
-    name = models.CharField(max_length=15)
-    created_at = models.DateField()
-    email = models.EmailField(unique=True)  # 이메일 중복 불가 설정
-
-class ShippingAddress(models.Model):
-    recipient = models.CharField(max_length=20)
-    phone_number = models.CharField(max_length=15)
-    destination = models.CharField(max_length=50)
-    postal_code = models.CharField(max_length=5)
-    address = models.CharField(max_length=50)
-    detail_address = models.CharField(max_length=50)
-    is_default = models.BooleanField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class UserPreferArt(models.Model):
-    created_at = models.DateField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    art_work = models.ForeignKey(ArtWork, on_delete=models.CASCADE)
-
-class University(models.Model):
-    name = models.CharField(max_length=255)
-    email_domain = models.CharField(max_length=255)
-
-class Department(models.Model):
-    name = models.CharField(max_length=20)
-    exhibit = models.ForeignKey(ArtExhibit, on_delete=models.CASCADE)
-    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+    email = models.EmailField(unique=True)
 
 class Seller(User):
     bank = models.CharField(max_length=20)
     bank_user = models.CharField(max_length=20)
     account = models.CharField(max_length=20)  # 계좌번호는 문자열로 처리
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    department = models.ForeignKey('Department')
+
+class ShippingAddress(models.Model):
+    recipient = models.CharField(max_length=20, null=True)
+    phone_number = models.CharField(max_length=15)
+    destination = models.CharField(max_length=50, null=True)
+    postal_code = models.CharField(max_length=5)
+    address = models.CharField(max_length=50)
+    detail_address = models.CharField(max_length=50)
+    is_default = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class UserPreferArt(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    art_work = models.ForeignKey(ArtWork, on_delete=models.CASCADE)
+
+class University(models.Model):
+    name = models.CharField(max_length=50)
+    email_domain = models.CharField(max_length=50, null=True)
+
+class Department(models.Model):
+    name = models.CharField(max_length=20)
+    university = models.ForeignKey(University, on_delete=models.CASCADE)

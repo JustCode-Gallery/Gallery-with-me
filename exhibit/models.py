ed84da658ba1b08a497e27948567916110f422ca
Yeonhui-ili <yeonhui@ewhain.net>
2024-07-08 13:33:50 +0900

    feat:exhibit, order의 모델 on-delete옵션 수정

--- exhibit/models.py
+++ exhibit/models.py
@@ -1,17 +1,18 @@
 from django.db import models
-from user.models import User
+from user.models import User, Department
 
 class ArtExhibit(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
-    address = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT) # SET_NULL?
 
 class ArtExhibitPoster(models.Model):
-    poster = models.ImageField(upload_to='posters/')
-    exhibit = models.ForeignKey(ArtExhibit, on_delete=models.CASCADE)
+    poster_url = models.URLField()
+    exhibit = models.ForeignKey(ArtExhibit, on_delete=models.CASCADE, related_name='images')
 
 class ExhibitBookmark(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    exhibit = models.ForeignKey(ArtExhibit, on_delete=models.CASCADE, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exhibit = models.ForeignKey(ArtExhibit, on_delete=models.CASCADE)

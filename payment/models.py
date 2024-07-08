7e56de70afa8816aaf83a9ccadb535a1bcc7ae78
SEYEON OH <seyeondoh@gmail.com>
2024-07-08 13:47:53 +0900

    payment app models

--- payment/models.py
+++ payment/models.py
@@ -6,32 +6,37 @@ class PaymentStatus(models.Model):
     status = models.CharField(max_length=10)
 
 class Payment(models.Model):
-    payment_uuid = models.CharField(max_length=50)
-    pay_method = models.CharField(max_length=20)
-    pay_date = models.DateTimeField()
-    pay_amount = models.DecimalField(max_digits=10, decimal_places=2)  # 금액은 DecimalField로 처리
-    pay_nobank_user = models.CharField(max_length=20)
-    pay_nobank = models.CharField(max_length=20)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    payment_status = models.ForeignKey(PaymentStatus, on_delete=models.CASCADE)
+    payment_uuid = models.CharField(max_length=50, unique=True, null=True)
+    pay_method = models.CharField(max_length=20) # 카카오페이, 카드, 무통장
+    pay_date = models.DateTimeField(null=True) # 실제 결제가 확인되면(payment_status가 '결제완료'로 바뀔 때) pay_date 필드 업데이트
+    pay_amount = models.DecimalField(decimal_places=2)
+    pay_escro_user = models.CharField(max_length=20, null=True) # 예금주
+    pay_escro = models.CharField(max_length=20, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) # user 객체 삭제되면 user=null, 카드정보로 찾기? (*카드번호 필드 부재)
+    payment_status = models.ForeignKey(PaymentStatus, on_delete=models.PROTECT) # PaymentStatus 객체 삭제 시도 시 ProtectError 내서 삭제할 수 없게
+
+    def __str__(self):
+        return f"{self.user.name}님의 결제정보" if self.user.id else "{self.payment_uuid}의 결제정보"
 
 class SettlementStatus(models.Model):
     status = models.CharField(max_length=10)
 
 class Settlement(models.Model):
-    settlement_date = models.DateTimeField()
-    updated_date = models.DateTimeField()
-    settlement_amount = models.DecimalField(max_digits=10, decimal_places=2)  # 금액은 DecimalField로 처리
-    set_account = models.CharField(max_length=20)  # 계좌번호는 문자열로 처리
-    set_bank = models.CharField(max_length=20)  # 은행 이름은 문자열로 처리
-    set_bank_user = models.CharField(max_length=20)
-    commission = models.DecimalField(max_digits=10, decimal_places=2)  # 금액은 DecimalField로 처리
-    vat = models.DecimalField(max_digits=10, decimal_places=2)  # 금액은 DecimalField로 처리
-    settlement_status = models.ForeignKey(SettlementStatus, on_delete=models.CASCADE)
-    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
-    order_item = models.ForeignKey(OrderItem, on_delete=models.CASCADE)
+    settlement_date = models.DateTimeField(auto_now_add=True) # 정산 요청 시 생성
+    updated_date = models.DateTimeField(auto_now=True, null=True) # 정산이 완료되면(settlement_status가 '처리완료'로 바뀔 때) updated_date 필드 업데이트
+    settlement_amount = models.DecimalField(decimal_places=2)
+    set_account = models.IntegerField(max_length=14, null=True) # 계좌번호
+    set_bank = models.CharField(max_length=20, null=True)
+    set_bank_user = models.CharField(max_length=20, null=True)
+    commission = models.DecimalField(max_digits=10, decimal_places=2)
+    vat = models.DecimalField(max_digits=10, decimal_places=2)
+    settlement_status = models.ForeignKey(SettlementStatus, on_delete=models.PROTECT)
+    seller = models.ForeignKey(Seller, on_delete=models.SET_NULL, null=True) # user 객체 삭제 시 계좌정보로 정산정보 조회
+    order_item = models.ForeignKey(OrderItem, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return f"{self.seller.name}님의 정산정보" if self.seller.id else f"{self.set_bank} {self.set_account}의 정산정보"
 
 class Commission(models.Model):
-    commission = models.DecimalField(max_digits=10, decimal_places=2)  # 금액은 DecimalField로 처리
-    vat = models.DecimalField(max_digits=10, decimal_places=2)  # 금액은 DecimalField로 처리
+    commission = models.DecimalField(max_digits=10, decimal_places=2)
+    vat = models.DecimalField(max_digits=10, decimal_places=2)

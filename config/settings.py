6edb561644c698e2d7ba3e39c56822c896e92cc5
Yeonhui-ili <yeonhui@ewhain.net>
2024-07-09 16:43:26 +0900

    fix:디비 연결 문제 해결

--- config/settings.py
+++ config/settings.py
@@ -143,7 +143,3 @@ STATICFILES_DIRS = [BASE_DIR/'static']
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Media
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- exhibit/views.py
+++ exhibit/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+import folium
 
-# Create your views here.
+
+map = folium.Map(location=[37, 127], zoom_start=10)

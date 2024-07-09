50a3bfb1b88eb0da3269a464bd98da168e3d7643
Yeonhui-ili <yeonhui@ewhain.net>
2024-07-09 17:33:02 +0900

    feat: 기본 전시 리스트, map 띄우기

--- exhibit/urls.py
+++ exhibit/urls.py
@@ -1,8 +1,10 @@
 # /exhibit/urls.py
 
 from django.urls import path
+from .views import *
 
 app_name = 'exhibit'
 urlpatterns = [
-
+    path('', exhibit_list, name='exhibit_list'),
+    path('map/<int:exhibit_id>/', exhibit_map, name='exhibit_map'),
 ]

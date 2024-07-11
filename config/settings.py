299d5f7eee95cbcea1100d3b0834fcd51adebcd3
YangJaegeon <jaeheony03@gmail.com>
2024-07-11 14:05:54 +0900

    'merge_setting'

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'user.User'
+
+LOGOUT_REDIRECT_URL = '/'   

b65166f6b49ffc97d82ffd0822485cb0b8749944
YangJaegeon <jaeheony03@gmail.com>
2024-07-14 20:59:11 +0900

    'user'

--- config/settings.py
+++ config/settings.py
@@ -151,3 +151,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'user.User'
 
 LOGOUT_REDIRECT_URL = '/'   
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.naver.com'
+EMAIL_PORT = 587    
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+

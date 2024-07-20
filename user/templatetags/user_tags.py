b5c788ef6f9f3169dd4b5b979fcc52864831f186
YangJaegeon <jaeheony03@gmail.com>
2024-07-20 13:52:32 +0900

    'update_seller_profile'

---
+++ user/templatetags/user_tags.py
@@ -0,0 +1,8 @@
+﻿from django import template
+from user.models import Seller
+
+register = template.Library()
+
+@register.simple_tag
+def verify_seller(user_id):
+    return Seller.objects.filter(id=user_id).exists()

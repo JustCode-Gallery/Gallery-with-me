95c81d372ff2526c389969f8cc17cd0934eb587c
GordPark <gord23p@gmail.com>
2024-07-24 10:16:55 +0900

    base 예약 링크연결, seller 예약기능 업데이트

--- board/urls.py
+++ board/urls.py
@@ -1,22 +1,21 @@
 # /board/urls.py
 
 from django.urls import path
-from .views import board_list, board_detail, board_create, board_create_form, temp_upload, form_submit, refresh_session, board_detail_edit, board_delete, board_update, board_search, exhibit_autocomplete
+from . import views 
 
 app_name = 'board'
 urlpatterns = [
-    path('', board_list, name='board_list'),
-    path('create/', board_create, name='board_create'),
-    path('create/form/', board_create_form, name='board_create_form'),
-    path('form_submit/', form_submit, name='form_submit'),
-    # path('detail/', board_detail, name='board_detail'),
-    path('detail/<int:pk>/', board_detail, name='board_detail'),
-    path('temp-upload/', temp_upload, name='temp_upload'),
-    path('refresh_session/', refresh_session, name='refresh_session'),
-    path('detail/<int:pk>/edit/', board_detail_edit, name='board_detail_edit'),
-    path('delete/<int:pk>/', board_delete, name='board_delete'),
-    path('update/<int:pk>/', board_update, name='board_update'),
-    path('board_search/', board_search, name='board_search'),
-    path('exhibit-autocomplete/', exhibit_autocomplete, name='exhibit-autocomplete'),
+    path('', views.board_list, name='board_list'),
+    path('create/', views.board_create, name='board_create'),
+    path('create/form/', views.board_create_form, name='board_create_form'),
+    path('form_submit/', views.form_submit, name='form_submit'),
+    path('detail/<int:pk>/', views.board_detail, name='board_detail'),
+    path('temp-upload/', views.temp_upload, name='temp_upload'),
+    path('refresh_session/', views.refresh_session, name='refresh_session'),
+    path('detail/<int:pk>/edit/', views.board_detail_edit, name='board_detail_edit'),
+    path('delete/<int:pk>/', views.board_delete, name='board_delete'),
+    path('update/<int:pk>/', views.board_update, name='board_update'),
+    path('board_search/', views.board_search, name='board_search'),
+    path('exhibit-autocomplete/', views.exhibit_autocomplete, name='exhibit-autocomplete'),
 
 ]
--- seller/views.py
+++ seller/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
-
+from order.models import Reservation
+from artwork.models import ArtWork, ArtImage
+from user.models import User,Seller
 # Create your views here.
+
+# 판매자 예약
+def seller_reserve(request): 
+    # 예약 작품 구매예약 조회
+    # 예약취소
+    # 예약 알림
+    # 예약일시 / 예약작품 / 예약중 / 결제금액 / 결제수단 / 예약번호?
+
+    ## 판매자인지 확인 
+    user = request.user
+    try:
+        seller = Seller.objects.get(id=user.id)
+    except Seller.DoesNotExist:
+            return render(request, 'home.html', {
+                'error_message': '등록된 판매자를 찾을 수 없습니다'
+            })
+    ## 등록한 작품 확인 > 없음 > 없음페이지
+    try:
+        artwork = ArtWork.objects.get(seller_id=seller.id)
+    except ArtWork.DoesNotExist:
+        return render(request,'seller/seller_noreserve.html')
+
+    ## 있음 > 예약 작품인지 확인 
+    #### 등록작품이 여러개일 경우 >> 추후개발
+    if artwork:
+        if artwork.is_reservable:
+            ## 예약 모델에 예약작품 있는지 확인 
+            reservation = Reservation.objects.get(user=user, art_work=artwork)
+            
+            artworks_with_images = []
+            images = ArtImage.objects.filter(artwork=artwork)
+            artworks_with_images.append({
+                    'artwork': artwork,
+                    'images': images
+                })
+        ## 예약 작품 아님 
+        else:
+            return render(request,'seller/seller_noreserve.html')
+    
+    context = {
+        'artworks_with_images': artworks_with_images,
+        'reservation': reservation,
+    }
+    
+    return render(request,'seller/seller_reserve.html', context)
+
+def seller_noreserve(request): 
+    return render(request,'seller/seller_noreserve')
+
+def reserve_cancel(request): 
+    return render(request,'seller/reserve_cancel.html')

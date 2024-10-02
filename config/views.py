5cb60d73120f4ff44f56be382001b229b360f82d
GordPark <gord23p@gmail.com>
2024-10-02 20:29:15 +0900

    home 작품추천 로그인/비로그인 분류, 이미지 분류기 Javascript, fastAPI연결

--- config/views.py
+++ config/views.py
@@ -4,10 +4,27 @@ from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from order.models import Cart
 
+from django.shortcuts import render, redirect
+
 class HomeView(ListView):
     model = User
     template_name = 'home.html'
     context_object_name = 'objects'
+
+    # 로그인 된 사용자 구분 
+    def get(self, request):
+        user = request.user
+        if user.is_authenticated:
+            login_user = user.username
+        else:
+            login_user = None
+        context = {
+            'login_user': login_user,
+        }
+        
+        return render(request, 'home.html', context)
+    
+        
 
 # 현재 사용자의 장바구니에 담긴 아이템의 수
 @login_required
